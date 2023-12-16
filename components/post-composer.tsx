7cdf5b8fcf46b4844761e26da9830fedd820ced2
Neer Patel <neer2005@gmail.com>
2023-12-15 20:00:49 -0500

    wip

--- components/post-composer.tsx
+++ components/post-composer.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { createPresignedS3Post, createPresignedS3Put } from "@/app/actions";
+import { createPresignedS3Put } from "@/app/actions";
 import { Paperclip } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function PostComposer() {
-  let file: File;
-  let postText = "";
+  const [text, setText] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
   async function onPost(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
-    if (!postText) {
+    if (!text) {
       return;
     }
     if (file) {
@@ -24,63 +26,63 @@ export default function PostComposer() {
       } else {
         console.error(`Upload failed: ${res}`);
       }
-      //   const { url, fields } = await createPresignedS3Post(); // server action
-      //   const formData = new FormData();
-
-      //   // add s3 fields
-      //   Object.entries(fields).forEach(([key, value]) => {
-      //     formData.append(key, value as string);
-      //   });
-
-      //   // add file
-      //   formData.append("file", file);
-
-      //   const uploadResponse = await fetch(url, {
-      //     method: "POST",
-      //     body: formData,
-      //   });
-
-      //   if (uploadResponse.ok) {
-      //     console.log("Upload success!");
-      //   } else {
-      //     console.error(`Upload failed: ${uploadResponse}`);
-      //   }
     }
   }
 
   function onAttachmentChange(event: React.ChangeEvent<HTMLInputElement>) {
     if (event.target.files && event.target.files.length > 0) {
-      file = event.target.files[0];
+      // file = event.target.files[0];
+      setFile(event.target.files[0]);
     }
   }
 
-  async function onPostTextChange(event: React.ChangeEvent<HTMLInputElement>) {
-    postText = event.target.value;
-    console.log(postText);
+  function onPostTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+    console.log(text);
   }
 
   return (
-    <form className="border-solid border-2 rounded border-white flex flex-col p-4 m-4">
-      <input
-        className="bg-black m-2"
-        type="text"
-        placeholder="Say it out!"
-        value={postText}
-        onChange={onPostTextChange}
-        required
-      ></input>
-      <div className="flex flex-row">
-        <label className="m-2">
-          <input
-            onChange={onAttachmentChange}
-            type="file"
-            accept="image/png, image/jpeg"
-            hidden
-          />
-          <Paperclip />
-        </label>
-        <button onClick={onPost}>Post</button>
+    <div>
+      <form className="border-solid border-2 rounded border-white flex flex-col p-4 m-4">
+        <input
+          className="bg-black m-2"
+          type="text"
+          placeholder="Say it out!"
+          value={text}
+          onChange={onPostTextChange}
+          required
+        ></input>
+        <div className="flex flex-row">
+          <label className="m-2">
+            <input
+              onChange={onAttachmentChange}
+              type="file"
+              accept="image/png, image/jpeg"
+              hidden
+            />
+            <Paperclip className="m-2" />
+          </label>
+          <button
+            className="p-2 m-2 hover:border-solid hover:border-2 hover:rounded hover:border-white"
+            disabled={text.length > 0}
+            onClick={onPost}
+          >
+            Post
+          </button>
+        </div>
+      </form>
+      <div>
+        {file && (
+          <div>
+            <Image
+              src={URL.createObjectURL(file)}
+              width={200}
+              height={200}
+              alt="attachment"
+            />
+          </div>
+        )}
       </div>
-    </form>
+    </div>
   );
 }
